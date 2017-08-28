e63f10f65916d8049531f8ff2e42f928f324d3c7
Hiroshi Doyu <hiroshi.doyu@ericsson.com>
2017-08-28 14:10:51 +0300

    client: sends 3 messages

--- client.go
+++ client.go
@@ -18,17 +18,19 @@ func main() {
 	}
 	defer conn.Close()
 
-	n, err := conn.Write([]byte("Ping"))
-	if err != nil {
-		log.Fatalln(err)
-		os.Exit(1)
-	}
+	buf := make([]byte, 1024)
+	for i := 0; i < 3; i++ {
+		n, err := conn.Write([]byte("Ping"))
+		if err != nil {
+			log.Fatalln(err)
+			os.Exit(1)
+		}
 
-	recvBuf := make([]byte, 1024)
-	n, err = conn.Read(recvBuf)
-	if err != nil {
-		log.Fatalln(err)
-		os.Exit(1)
+		n, err = conn.Read(buf)
+		if err != nil {
+			log.Fatalln(err)
+			os.Exit(1)
+		}
+		log.Printf("%d: Received data: %s", i, string(buf[:n]))
 	}
-	log.Printf("Received data: %s", string(recvBuf[:n]))
 }

8ccb142b19afce5040581909ba99606c431bd099
Hiroshi Doyu <hiroshi.doyu@ericsson.com>
2017-08-28 11:54:23 +0300

    initial commit

---
+++ server.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"log"
+	"net"
+	"os"
+)
+
+func reply(conn *net.UDPConn, addr *net.UDPAddr) {
+	log.Printf("Sending data..")
+	conn.WriteTo([]byte("Pong"), addr)
+	log.Printf("Complete Sending data..")
+}
+
+func main() {
+	src := &net.UDPAddr{
+		IP:   net.ParseIP("127.0.0.1"),
+		Port: 8080,
+	}
+	conn, err := net.ListenUDP("udp", src)
+	if err != nil {
+		log.Fatalln(err)
+		os.Exit(1)
+	}
+
+	buf := make([]byte, 1024)
+	log.Println("Starting udp server...")
+	for {
+		n, addr, err := conn.ReadFromUDP(buf)
+		if err != nil {
+			log.Fatalln(err)
+			os.Exit(1)
+		}
+		log.Printf("Reciving data: %s from %s", string(buf[:n]), addr.String())
+		go reply(conn, addr)
+	}
+}
+

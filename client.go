3fa1c09fa60027d465ecb3bfd1b85e04b825db9e
Hiroshi Doyu <hiroshi.doyu@ericsson.com>
2017-08-29 10:43:31 +0300

    client: introduce go routine

--- client.go
+++ client.go
@@ -6,31 +6,38 @@ import (
 	"os"
 )
 
+func ping(conn *net.UDPConn, done chan bool, idx int) {
+	n, err := conn.Write([]byte("Ping"))
+	if err != nil {
+		log.Fatalln(err)
+		os.Exit(1)
+	}
+
+	buf := make([]byte, 1024)
+	n, err = conn.Read(buf)
+	if err != nil {
+		log.Fatalln(err)
+		os.Exit(1)
+	}
+	log.Printf("%d: Received data: %s", idx, string(buf[:n]))
+	done <- true
+}
+
 func main() {
 	dst := &net.UDPAddr{
 		IP:   net.ParseIP("127.0.0.1"),
 		Port: 8080,
 	}
-	conn, err := net.Dial("udp", dst.String())
+	conn, err := net.DialUDP("udp", nil, dst)
 	if err != nil {
 		log.Fatalln(err)
 		os.Exit(1)
 	}
 	defer conn.Close()
 
-	buf := make([]byte, 1024)
+	done := make(chan bool, 1)
 	for i := 0; i < 3; i++ {
-		n, err := conn.Write([]byte("Ping"))
-		if err != nil {
-			log.Fatalln(err)
-			os.Exit(1)
-		}
-
-		n, err = conn.Read(buf)
-		if err != nil {
-			log.Fatalln(err)
-			os.Exit(1)
-		}
-		log.Printf("%d: Received data: %s", i, string(buf[:n]))
+		ping(conn, done, i)
+		<-done
 	}
 }
